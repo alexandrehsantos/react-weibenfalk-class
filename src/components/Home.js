be01c2fbf42c8921a62e1cabcf5c13b996735f97
Alexandre Santos <alexandrehsantos@gmail.com>
2022-09-13 22:00:11 -0300

    feat: add hero image, header, grid and thumb

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import API from '../API'
+import React, { useState, useEffect } from "react";
+import API from "../API";
 
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from '../config';
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL, API_KEY } from "../config";
 //Components
+import HeroImage from "./HeroImage";
+import Grid from "./Grid";
+import Thumb from "./Thumb";
 
-//Hook 
+//Hook
+import { useHomeFetch } from "../hooks/useHomeFetch";
 
 //Image
-import NoImage from '../images/no_image.jpg'
+import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-    //Hook -> useState()
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
+  console.log(loading);
 
-    const fetchMovies = async (page, searchTerm = "") => {
-        try {
-            setError(false);
-            setLoading(true);
+  return (
+    <>
+      {" "}
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <Thumb key={movie.id}
+            clickable = {true}
+            image= {
+              movie.poster_path 
+               ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path 
+               : NoImage
+            }
+            movieId={movie.id}
+          />
+        ))}
 
-            const movies = await API.fetchMovies(searchTerm, page);
-            
-            setState(prev => ({
-                ...movies,
-                results:
-                    page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-
-            }))
-        }catch (error){
-            setError(true);
-        }
-        setLoading(false);
-    };
-
-    //Initial render
-    useEffect(() => {
-        
-        fetchMovies(1);
-
-        
-    }, []);
-
-    console.log(state);
-
-    return <div>Home Page</div>
+      </Grid>
+    </>
+  );
 };
-
 
 export default Home;
